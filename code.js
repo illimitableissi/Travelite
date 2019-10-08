8b35a2cfa85553e44bda1352d912359af22e26d4
Andrew Fowose <usafowose@gmail.com>
2019-10-08 19:46:08 -0400

    Local Drew Chnges

--- code.js
+++ code.js
@@ -11,7 +11,25 @@ var settings = {
     "x-access-token": "15b8d8251f337fc45164a04b18f5025a"
   }
 };
-
 $.ajax(settings).done(function(response) {
-  console.log(response);
+  var priceRubles = response.data.ATL[0].price
+  console.log(priceRubles);
+  var priceDollars = Math.floor(priceRubles * 0.01539);
+  var $card =$('.card-title');
+  $card.text('$'+priceDollars+ ' USD'); 
+});
+
+var weather = {
+	async: true,
+	crossDomain: true,
+	url: "https://community-open-weather-map.p.rapidapi.com/weather?callback=test&lang=en&units=imperial&mode=JSON&q=Baltimore",
+	method: "GET",
+	headers: {
+		"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+		"x-rapidapi-key": "c6133bf5c4msh6e25171d00ff6f5p1d19fbjsn05887a4d5fe0"
+	}
+}   
+
+$.ajax(weather).done(function(answer) {
+	console.log(answer.MAIN);
 });
