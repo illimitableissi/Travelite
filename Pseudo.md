a2cfff574df2da48ffa2bb8d70b761e95729defc
Andrew Fowose <usafowose@gmail.com>
2019-10-08 21:29:34 -0400

    Updating Psuedo and Readme

--- Pseudo.md
+++ Pseudo.md
@@ -1,4 +1,6 @@
                                                 INDEX.HTML PSUEDO
-1. Basic HTML template with jquery/bootstrap CDN and links to external sheets (JS and CSS)
-2. Bootstrap Containers and Div's (Different Bootstrap Elements Used)
- - Div/Bootstrap Card
+1. Basic HTML template with jquery/materialize CDN and links to external sheets (JS and CSS)
+2. Materialize Containers and Div's (Different Materialize Elements Used)
+ - Div/Materialize Card
+ - Class names and Input 
+3. 
--- code.js
+++ code.js
@@ -1,35 +1,20 @@
 var settings = {
-  async: true,
   crossDomain: true,
-  url:
-    "https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v1/prices/direct/?destination=ATL&origin=LAX",
-  method: "GET",
   headers: {
-    "x-rapidapi-host":
-      "travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com",
-    "x-rapidapi-key": "c6133bf5c4msh6e25171d00ff6f5p1d19fbjsn05887a4d5fe0",
     "x-access-token": "15b8d8251f337fc45164a04b18f5025a"
-  }
+  },
+  url: "http://api.travelpayouts.com/v1/prices/cheap?origin=ATL&destination=LAX&depart_date=2019-10&return_date=2019-10&token=15b8d8251f337fc45164a04b18f5025a",
+  method: "GET"
 };
-$.ajax(settings).done(function(response) {
-  var priceRubles = response.data.ATL[0].price
-  console.log(priceRubles);
-  var priceDollars = Math.floor(priceRubles * 0.01539);
-  var $card =$('.card-title');
-  $card.text('$'+priceDollars+ ' USD'); 
-});
-
-var weather = {
-	async: true,
-	crossDomain: true,
-	url: "https://community-open-weather-map.p.rapidapi.com/weather?callback=test&lang=en&units=imperial&mode=JSON&q=Baltimore",
-	method: "GET",
-	headers: {
-		"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-		"x-rapidapi-key": "c6133bf5c4msh6e25171d00ff6f5p1d19fbjsn05887a4d5fe0"
-	}
-}   
-
-$.ajax(weather).done(function(answer) {
-	console.log(answer.MAIN);
-});
+$.ajax(settings)
+  .then(function(response) {
+    debugger;
+    console.log(response);
+    //   var priceRubles = response.data;
+    //   console.log(priceRubles);
+    //   var priceDollars = Math.floor(priceRubles * 0.01539);
+    //   var $card = $(".card-title");
+    //   $card.text("$" + priceDollars + " USD");
+    //   console.log(`The price of the flight is $${priceDollars} USD`);
+  })
+  .fail(err => console.log(err));
