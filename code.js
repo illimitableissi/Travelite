2760d2aec5795c77e2f26fb60e89ff3b12b213c9
SOVEREIGN <isimemeni@gmail.com>
2019-10-10 00:13:58 -0400

    fixes

--- code.js
+++ code.js
@@ -5,6 +5,7 @@ var settings = {
     "http://api.travelpayouts.com/v1/prices/monthly?currency=USD&origin=ATL&destination=LAX&token=15b8d8251f337fc45164a04b18f5025a",
   method: "GET"
 };
+
 $.ajax(settings).done(function(response) {
   console.log(response);
   var priceRubles = response.data;
