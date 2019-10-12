3a4d49bf91932eb3b26811123620fd0ff5d541b9
Colin Green <colin.green@knights.ucf.edu>
2019-10-12 13:01:48 -0400

    Removed an input field from index, added stuff to table on flight.js

--- flight.js
+++ flight.js
@@ -12,21 +12,29 @@ var settings = {
    $.ajax(settings).done(function(response) {
     console.log(response);
     var data = response.data;
+
     console.log(data);
-    var myTable = "<tr><td>Flight Number</td><td>Price</td><td>Departs</td><td>Arrives</td></tr>";
+
+    var myTable = "<tr><td>Flight Number</td><td>Destination</td><td>Price</td><td>Departs</td><td>Arrives</td><td>Airline</td></tr>";
+
     myTable = appendMonthData(myTable, data["2019-10"]);
     myTable = appendMonthData(myTable, data["2019-11"]);
     myTable = appendMonthData(myTable, data["2019-12"]);
     myTable = appendMonthData(myTable, data["2020-01"]);
-    var myDisplayArea = $("#travel-api-display");
-    myDisplayArea.append(myTable);
+    var myDisplayArea = $("#travel-api-results");
+    myDisplayArea.html(myTable);
    });
    function appendMonthData(myTableString, myMonthlyDataToAppend)
    {
+    var departureTime = moment(myMonthlyDataToAppend.departure_at);
+    var returnTime = moment(myMonthlyDataToAppend.return_at);
+
     myTableString = myTableString +
       "<tr><td>" + myMonthlyDataToAppend.flight_number + "</td>" +
+      "<td>" + myMonthlyDataToAppend.destination + "</td>" +
       "<td>" + "$" + myMonthlyDataToAppend.price + "</td>" +
-      "<td>" + myMonthlyDataToAppend.departure_at + "</td>" +
-      "<td>" + myMonthlyDataToAppend.return_at + "</td>" + "</tr>";
+      "<td>" + departureTime.format('MMMM Do YYYY, h:mm A') + "</td>" +
+      "<td>" + returnTime.format('MMMM Do YYYY, h:mm A') + "</td>" +
+      "<td>" + myMonthlyDataToAppend.airline + "</td>" + "</tr>";
     return myTableString;
     }
